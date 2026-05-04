1b89a20ad40d38effe2a8d2d44aacfae0e27faa2
mustafa.ozturk <musertac@hotmail.com>
2026-05-04 19:23:49 +0300

    +++

--- app/component/Home/Navbar/Nav.tsx
+++ app/component/Home/Navbar/Nav.tsx
@@ -10,8 +10,8 @@ type NavProps = {
 }
 const Nav = ({ openNav }: NavProps) => {
     return (
-        <div className="relative z-20 h-[10vh] sm:h-[11vh] lg:h-[12vh] lg:mt-6">
-            <div className="mx-auto bg-transparent flex h-full w-[92%] items-center justify-between xl:w-[80%]">
+        <div className="bg-[#1f242c] relative z-20 h-[10vh] sm:h-[11vh] lg:h-[12vh] ">
+            <div className="mx-auto flex bg-[#1f242c] h-full w-[92%] items-center justify-between xl:w-[80%]">
 
                 <Logo></Logo>
                 <div className='hidden h-[9vh] items-center space-x-7 bg-white pl-4 pr-2 lg:flex xl:space-x-10'>
