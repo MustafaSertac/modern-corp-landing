e51b1a2dbd6c1dbe2e6b0aa4ccb5f7a800680bad
mustafa.ozturk <musertac@hotmail.com>
2026-05-04 19:32:58 +0300

    Revert "+++"
    This reverts commit 1b89a20ad40d38effe2a8d2d44aacfae0e27faa2.

--- app/component/Helper/Logo.tsx
+++ app/component/Helper/Logo.tsx
@@ -3,7 +3,7 @@ import { BiSolidBusiness } from 'react-icons/bi'
 
 const Logo = () => {
     return (
-        <div className=" flex items-center space-x-2">
+        <div className="flex items-center space-x-2">
 
             <div className="w-10 h-10 bg-[#b69974] rounded-full flex items-center justify-center">
                 <BiSolidBusiness className="text-white w-6 h-6" />
--- app/component/Home/Hero/Hero.tsx
+++ app/component/Home/Hero/Hero.tsx
@@ -7,21 +7,21 @@ const Hero = () => {
         {/*Text Comment */}
     <div>
         {/*Title */}
-        <h1 className='text-4xl md:text-7xl font-bold text-white'  >
+        <h1 className='text-4xl md:text-7xl font-bold text-white' data-aos="fade-up">
             Crafting your digital 
             <br></br>story with passion
         </h1>
     </div>
     {/*Description */}
-       <p  data-aos="fade-up"   className='max-w-150 ml-0 md:ml-12 md:mt-10 mt-5 text-sm md:text-base font-semibold'>
+       <p  data-aos="fade-up" data-aos-delay="180" className='max-w-150 ml-0 md:ml-12 md:mt-10 mt-5 text-sm md:text-base font-semibold'>
         Payment solutions enable businesses to accept payments 
         Payment solutions enable businesses to accept payments from customers
          conveniently securely from customers conveniently and securely.
         </p>
-        <button data-aos="fade-up" className="bg-[#1f242c] px-8 py-3 rounded-full mt-10 ml-0 md:ml-12 text-white font-semibold cursor-pointer hover:bg-[#9f7c2d] transition-all duration-300 outline-2 outline-gray-300 w-fit">
+        <button data-aos="fade-up" data-aos-delay="180" className="bg-[#1f242c] px-8 py-3 rounded-full mt-10 ml-0 md:ml-12 text-white font-semibold cursor-pointer hover:bg-[#9f7c2d] transition-all duration-300 outline-2 outline-gray-300 w-fit">
   Discover More
 </button> </div>
-<div className='absolute top-0 right-48 hidden lg:block '>
+<div className='absolute top-0 right-48 hidden lg:block ' data-aos="zoom-in-left" data-aos-delay="200">
 
     <img src="/images/hero.png" alt="Hero Image" width={550} height={550} />
 </div>
